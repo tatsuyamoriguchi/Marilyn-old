63c394410e1a2b429028881b59a0dff2ba03c591
tatsuyamoriguchi <caliosdev@gmail.com>
2019-04-18 10:00:47 -0700

    Initial Commit

---
+++ Marilyn/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Marilyn
+//
+//  Created by Tatsuya Moriguchi on 4/18/19.
+//  Copyright © 2019 Becko's Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Marilyn/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Marilyn
+//
+//  Created by Tatsuya Moriguchi on 4/18/19.
+//  Copyright © 2019 Becko's Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
